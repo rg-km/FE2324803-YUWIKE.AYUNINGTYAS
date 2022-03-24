27f2a9cf58472eab3cdb86b47a39f430906e893e
Yuwike Ayuningtyas <90737526+YuwikeA@users.noreply.github.com>
2022-03-24 15:24:34 +0700

    Workshop-Javascript 3

---
+++ function-cp/script-1.js
@@ -0,0 +1,21 @@
+// Mengembalikan teks yang berkebalikan
+//
+// contoh:
+// teks = hello world
+// hasil:
+// dlrow olleh
+
+function reverseString(str) {
+  var currentString = str;
+  var newString = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    newString = newString + currentString[i];
+  }
+
+  return newString;
+}
+
+const string = prompt("Masukan teks: ");
+
+const result = reverseString(string);
+console.log(result);

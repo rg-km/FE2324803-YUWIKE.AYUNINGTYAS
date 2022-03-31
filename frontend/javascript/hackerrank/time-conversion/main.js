7dac4c381370ad82a69e962f6ebd68f3bdc8f7a8
Yuwike Ayuningtyas <sleepy06yuwike@gmail.com>
2022-03-31 09:58:29 +0700

    time-conversion

--- frontend/javascript/hackerrank/time-conversion/main.js
+++ frontend/javascript/hackerrank/time-conversion/main.js
@@ -14,23 +14,40 @@
 function timeConversion(s) {
   // Write your code here
   // TODO: answer here
-  let ampm = s[s.length - 2] + s[s.length - 1];
-  let hour = Number(s[0] + s[1]);
-
-  if (ampm === "AM") {
-    if (hour > 12) {
-      hour = 0;
-    } else if (ampm === "PM") {
-      if (hour !== 12) {
-        hour = 12 + hour;
-      }
+  var time = s.split(":");
+  var hour = parseInt(time[0]);
+  var minute = time[1];
+  var second = time[2].split("");
+  var ampm = second[second.length - 2] + second[second.length - 1];
+  if (ampm === "PM") {
+    if (hour !== 12) {
+      hour += 12;
+    }
+  } else {
+    if (hour === 12) {
+      hour = "00";
     }
   }
-  let result = `${hour}`;
-  for (let i = 2; i < s.length - 2; i++) {
-    result += s[i];
-  }
-  return result;
+  second.splice(second.length - 2, 2);
+  return hour + ":" + minute + ":" + second.join("");
+
+  // let ampm = s[s.length - 2] + s[s.length - 1];
+  // let hour = Number(s[0] + s[1]);
+
+  // if (ampm === "AM") {
+  //   if (hour > 12) {
+  //     hour = 0;
+  //   } else if (ampm === "PM") {
+  //     if (hour !== 12) {
+  //       hour = 12 + hour;
+  //     }
+  //   }
+  // }
+  // let result = `${hour}`;
+  // for (let i = 2; i < s.length - 2; i++) {
+  //   result += s[i];
+  // }
+  // return result;
 }
 
 function main() {

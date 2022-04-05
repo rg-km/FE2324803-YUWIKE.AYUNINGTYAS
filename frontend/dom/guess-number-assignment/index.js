946a65cc74e07d116e4c5de07b871e062900fb91
Yuwike Ayuningtyas <sleepy06yuwike@gmail.com>
2022-04-05 10:04:55 +0700

    new

--- frontend/dom/guess-number-assignment/index.js
+++ frontend/dom/guess-number-assignment/index.js
@@ -10,22 +10,39 @@ Uncomment variable dibawah ini untuk mulai mengerjakan. dilarang mengganti nama 
 // let checkBtn = tombol untuk melakuan pengecekan angka pada input
 // let score = nilai yang akan ditampilakan
 
+let quote = document.getElementById("random-quote");
+let author = document.getElementsByClassName("author");
+let citation = document.getElementsByClassName("citation");
+let year = document.getElementsByClassName("year");
+let button = document.getElementsByTagName("button");
+
 // TODO: answer here
+button[0].addEventListener("click", () => {
+  const n = getQuote();
+  displayQuote(n);
+});
 
-
-function displayMessage(msg) {
-  // TODO: answer here
+function getQuote() {
+  const index = Math.floor(Math.random() * (quotes.length - 1));
+  return index;
 }
 
-function resetGame() {
-  // TODO: answer here
-  //dilarang menghapus code dibawah ini!
-  document.getElementById('hidden-number').innerHTML = randomNumber
-}
-
-function checkNumber() {
-  // TODO: answer here
+function displayQuote(n) {
+  const currQuote = quotes[n];
+  quote.innerText = currQuote.quote;
+  author[0].innerText = currQuote.author;
+  if (currQuote.hasOwnProperty("citation")) {
+    citation[0].innerText = currQuote.citation;
+  } else {
+    citation[0].innerText = "";
+  }
+  if (currQuote.hasOwnProperty("year")) {
+    year[0].innerText = currQuote.year;
+  } else {
+    year[0].innerText = "";
+  }
+  console.log(currQuote);
 }
 
 //dilarang menghapus code dibawah ini!
-document.getElementById('hidden-number').innerHTML = randomNumber
+document.getElementById("hidden-number").innerHTML = randomNumber;

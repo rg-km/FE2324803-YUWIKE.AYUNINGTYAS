e2f75d189c341101756338f216b391bbc7f12221
Yuwike Ayuningtyas <sleepy06yuwike@gmail.com>
2022-04-05 08:32:29 +0700

    test

--- frontend/dom/stopwatch-app-assignment/index.js
+++ frontend/dom/stopwatch-app-assignment/index.js
@@ -8,20 +8,47 @@ Uncomment variable dibawah ini untuk mulai mengerjakan. dilarang mengganti nama 
 // let resetbtn = tombol untuk mereset value dari stopwatch
 
 // TODO: answer here
-
+let stopwatch = document.getElementById("stopwatch");
+let startbtn = document.getElementById("start");
+let stopbtn = document.getElementById("stop");
+let resetbtn = document.getElementById("reset");
 
 function start() {
   // TODO: answer here
+  let startTime = new Date();
+  let startTimeString = startTime.toLocaleTimeString();
+  stopwatch.innerHTML = startTimeString;
+  let interval = setInterval(function () {
+    let currentTime = new Date();
+    let currentTimeString = currentTime.toLocaleTimeString();
+    stopwatch.innerHTML = currentTimeString;
+  }, 1000);
 }
 
 function stop() {
   // TODO: answer here
+  let stopTime = new Date();
+  let stopTimeString = stopTime.toLocaleTimeString();
+  stopwatch.innerHTML = stopTimeString;
+  clearInterval(interval);
 }
 
 function reset() {
   // TODO: answer here
+  let resetTime = new Date();
+  let resetTimeString = resetTime.toLocaleTimeString();
+  stopwatch.innerHTML = resetTimeString;
+  clearInterval(interval);
 }
 
 function runTime() {
   // TODO: answer here
+  let startTime = new Date();
+  let startTimeString = startTime.toLocaleTimeString();
+  stopwatch.innerHTML = startTimeString;
+  let interval = setInterval(function () {
+    let currentTime = new Date();
+    let currentTimeString = currentTime.toLocaleTimeString();
+    stopwatch.innerHTML = currentTimeString;
+  }, 1000);
 }

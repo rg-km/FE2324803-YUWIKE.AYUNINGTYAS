1d051eedeea521545db43a2729652dcd78b95745
Yuwike Ayuningtyas <sleepy06yuwike@gmail.com>
2022-04-12 16:03:55 +0700

    oop steal update

--- frontend/javascript/11-oop-steal-gold-cp/index.js
+++ frontend/javascript/11-oop-steal-gold-cp/index.js
@@ -6,47 +6,59 @@ class Player {
     this.hasBeenRobbed = false;
   }
 
-
   getGold() {
-  // TODO: answer here
+    // TODO: answer here
+    return this.gold;
   }
 
   setGold(gold) {
-  // TODO: answer here
+    // TODO: answer here
+    this.gold = gold;
   }
 
   getStealChance() {
-  // TODO: answer here
+    // TODO: answer here
+    return this.stealChance;
   }
 
   setStealChance(chance) {
-  // TODO: answer here
+    // TODO: answer here
+    this.stealChance = chance;
   }
 
   getHasBeenRobbed() {
     return this.hasBeenRobbed;
   }
-  
+
   setHasBeenRobbed(status) {
     this.hasBeenRobbed = status;
   }
-  
+
   randomizer() {
     return +Math.random().toFixed(2);
   }
-  
+
   steal(player) {
     // TODO: answer here
+    if (player.gold < 5) {
+      return "Lawan terlalu miskin";
+    } else if (this.randomizer() < this.getStealChance()) {
+      player.setGold(player.getGold() - 5);
+      this.setGold(this.getGold() + 5);
+      this.setHasBeenRobbed(true);
+      return "Berhasil mencuri 5 gold";
+    }
+    return "Gagal mencuri, coba lain kali";
   }
 }
 
 const p1 = new Player("Fauzan");
 const p2 = new Player("Tegar");
 
-console.log('Jumlah gold Player 1: ' + p1.getGold())
-console.log('Jumlah gold Player 2: ' + p2.getGold())
+console.log("Jumlah gold Player 1: " + p1.getGold());
+console.log("Jumlah gold Player 2: " + p2.getGold());
 console.log(p1.steal(p2));
-console.log('Total gold Player 1: ' + p1.getGold())
-console.log('Total gold Player 2: ' + p2.getGold())
+console.log("Total gold Player 1: " + p1.getGold());
+console.log("Total gold Player 2: " + p2.getGold());
 
 module.exports = Player;

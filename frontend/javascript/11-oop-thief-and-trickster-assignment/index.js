497eb2f7e72866f17ea8da7bccedb47b5e974dd8
Yuwike Ayuningtyas <sleepy06yuwike@gmail.com>
2022-04-13 10:57:34 +0700

    update 11-oop-thief-and-trickster-assignment

--- frontend/javascript/11-oop-thief-and-trickster-assignment/index.js
+++ frontend/javascript/11-oop-thief-and-trickster-assignment/index.js
@@ -5,13 +5,40 @@ class Thief extends Player {
     super(name);
     this.job = "Thief";
   }
-  
+
   robbedBlind() {
     // TODO: answer here
+    if (this.getGold() < 10) {
+      return "Aku terlalu miskin";
+    } else {
+      this.setGold(this.getGold() - 10);
+      return this.setStealChance(0.75);
+    }
   }
 
   steal(player) {
     // TODO: answer here
+    if (player.randomizer() <= this.getStealChance() && player.getGold() >= 5) {
+      let goldTemp1 = 0;
+      let goldTemp2 = 0;
+      goldTemp1 = this.getGold() + 5;
+      goldTemp2 = player.getGold() - 5;
+      this.setGold(goldTemp1);
+      player.setGold(goldTemp2);
+      if (player.job === "Trickster") {
+        player.setHasBeenRobbed(false);
+        return player.distractionPurse(this);
+      } else {
+        player.setHasBeenRobbed(true);
+        return "Berhasil mencuri 5 gold";
+      }
+    } else if (player.getGold() < 5) {
+      player.setHasBeenRobbed(false);
+      return "Lawan terlalu miskin";
+    } else {
+      player.setHasBeenRobbed(false);
+      return "Gagal mencuri, coba lain kali";
+    }
   }
 }
 
@@ -24,19 +51,35 @@ class Trickster extends Player {
 
   setDistractionPurseChance(chance) {
     // TODO: answer here
+    this.distractionPurseChance = chance;
   }
 
   getDistractionPurseChance() {
     // TODO: answer here
+    return this.distractionPurseChance;
   }
 
   distractionPurse(player) {
     const rng = this.randomizer();
     // TODO: answer here
+    if (this.getDistractionPurseChance() > rng) {
+      return this.steal(player);
+    } else {
+      return "Gagal mencuri balik";
+    }
   }
 
   steal(player) {
     // TODO: answer here
+    if (player.getGold() > 10) {
+      this.setGold(this.getGold() + 10);
+      player.setGold(player.getGold() - 10);
+      return "Berhasil mencuri balik 10 gold";
+    } else {
+      this.setGold(this.getGold() + player.getGold());
+      player.setGold(0);
+      return "Berhasil mencuri balik semua uang lawan";
+    }
   }
 }
 

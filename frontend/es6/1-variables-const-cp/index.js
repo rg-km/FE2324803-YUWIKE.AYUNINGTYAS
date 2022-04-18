70bb4dc409a88b48b4ee1597ee39f032d6212916
Yuwike Ayuningtyas <sleepy06yuwike@gmail.com>
2022-04-18 07:51:10 +0700

    variable-const

--- frontend/es6/1-variables-const-cp/index.js
+++ frontend/es6/1-variables-const-cp/index.js
@@ -4,7 +4,10 @@
 function constantNoChange() {
   const kota = "Tanggerang";
   // TODO: answer here
+  if (true) {
+    kota = "Jakarta";
+  }
+  return kota;
 }
 
-module.exports = constantNoChange
-
+module.exports = constantNoChange;

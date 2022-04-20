486f095603a3a81f35b3dcd741d905658f4a7fef
Yuwike Ayuningtyas <sleepy06yuwike@gmail.com>
2022-04-20 07:45:32 +0700

    10-async-cp

--- frontend/javascript/10-async-cp/index.js
+++ frontend/javascript/10-async-cp/index.js
@@ -1,50 +1,52 @@
 /**
  * Buatlah fungsi async yang tugasnya mengambil data karakter starwars dan mengembalikannya dalam bentuk string sebagai berikut:
- * 
+ *
  * Input: 1 // Karakter Id.
  * Output: "Luke Skywalker, memiliki tinggi 175cm dan lahir pada tahun 19BBY"
- * 
+ *
  * Untuk informasi yang lebih lengkap mengenai endpoint yang digunakan, silahkah akses https://swapi.dev/documentation#people
- * 
+ *
  * Gunakan keyword async/await untuk menyelesaikan fungsi ini.
  */
 
 /**
  * Gunakan fungsi berikut untuk mempermudah http request.
  */
- const https = require("https");
+const https = require("https");
 
- function getStarWarsData(url) {
-   return new Promise((resolve, reject) => {
-     https
-       .get(url, (res) => {
-         let result = "";
- 
-         if (res.statusCode !== 200) {
-           reject(new Error(res.statusCode));
-         }
- 
-         res.on("data", (d) => {
-           result += d;
-         });
- 
-         res.on("end", () => {
-           resolve(result);
-         });
-       })
-       .on("error", (e) => {
-         reject(e);
-       });
-   });
- }
- 
- 
- async function getDataPeopleById(id) {
-   // TODO: answer here
- }
+function getStarWarsData(url) {
+  return new Promise((resolve, reject) => {
+    https
+      .get(url, (res) => {
+        let result = "";
 
- module.exports = {
-  getDataPeopleById
- }
- 
- 
+        if (res.statusCode !== 200) {
+          reject(new Error(res.statusCode));
+        }
+
+        res.on("data", (d) => {
+          result += d;
+        });
+
+        res.on("end", () => {
+          resolve(result);
+        });
+      })
+      .on("error", (e) => {
+        reject(e);
+      });
+  });
+}
+
+async function getDataPeopleById(id) {
+  // TODO: answer here
+  const url = `https://swapi.dev/api/people/${id}`;
+  const result = await getStarWarsData(url);
+  const data = JSON.parse(result);
+  const { name, height, birth_year } = data;
+  return `${name}, memiliki tinggi ${height}cm dan lahir pada tahun ${birth_year}`;
+}
+
+module.exports = {
+  getDataPeopleById,
+};

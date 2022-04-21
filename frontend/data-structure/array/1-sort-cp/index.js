956e72463db9a3c5d99471e3ac7d3100dfde33dc
Yuwike Ayuningtyas <sleepy06yuwike@gmail.com>
2022-04-22 03:48:15 +0700

    filecp&assignment

--- frontend/data-structure/array/1-sort-cp/index.js
+++ frontend/data-structure/array/1-sort-cp/index.js
@@ -13,17 +13,33 @@
 // [1,2,3,4,5] -> [2,3,4,5,1] -> [3,4,5,1,2] -> [4,5,1,2,3] -> [5,1,2,3,4]
 
 function sort(arr) {
-    return arr // TODO: replace this
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] > arr[j]) {
+        let temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  return arr; // TODO: replace this
 }
 
 function rotateLeft(d, arr) {
-    return arr // TODO: replace this
+  let temp = [];
+  for (let i = 0; i < d; i++) {
+    temp.push(arr.pop());
+  }
+  for (let i = 0; i < temp.length; i++) {
+    arr.unshift(temp[i]);
+  }
+  return arr; // TODO: replace this
 }
 
-let nums = [4, 5, 2, 1, 3]
-let sorted = sort(nums)
-console.log(sorted)
-rotated = rotateLeft(4, sorted)
-console.log(rotated)
+let nums = [4, 5, 2, 1, 3];
+let sorted = sort(nums);
+console.log(sorted);
+rotated = rotateLeft(4, sorted);
+console.log(rotated);
 
-module.exports = { sort, rotateLeft }
+module.exports = { sort, rotateLeft };

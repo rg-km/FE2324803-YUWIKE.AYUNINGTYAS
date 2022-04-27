07cebc49d10c3256d0858a49be3a4460cb55ba5c
Yuwike Ayuningtyas <sleepy06yuwike@gmail.com>
2022-04-27 22:40:12 +0700

    assigment text editor

--- frontend/data-structure/assignment/text-editor/index.js
+++ frontend/data-structure/assignment/text-editor/index.js
@@ -1,21 +1,51 @@
 module.exports = class TextEditor {
-    constructor() {
-        // TODO: answer here
-    }
+  constructor() {
+    // TODO: answer here
+    this.undoStack = "";
+    this.redoStack = "";
+    this.undoArrStack = [];
+    this.redoArrStack = [];
+  }
 
-    write(c) {
-        // TODO: answer here
-    }
+  write(c) {
+    // TODO: answer here
+    this.undoStack += c; //menyiman data yang diinputkan
+    this.undoArrStack.push(c); //menyimpan data yang diinputkan di c
+  }
 
-    read() {
-        // TODO: answer here
+  read() {
+    // TODO: answer here
+    this.undoStack = "";
+    for (let i = 0; i < this.undoArrStack.length; i++) {
+      this.undoStack += this.undoArrStack[i];
     }
+    return this.undoStack;
+  }
 
-    undo() {
-        // TODO: answer here
+  undo() {
+    // TODO: answer here
+    if (this.undoStack.length != 0) {
+      var poopedValue = this.undoArrStack.pop();
+      this.redoArrStack.push(poopedValue);
+      this.undoStack = "";
+      this.redoStack += poopedValue;
+      for (let i = 0; i < this.undoArrStack.length; i++) {
+        this.undoStack += this.undoArrStack[i];
+      }
     }
-    
-    redo() {
-        // TODO: answer here
+  }
+
+  redo() {
+    // TODO: answer here
+    var poopedValue = this.redoArrStack.pop();
+    this.undoArrStack.push(poopedValue);
+    this.redoStack = "";
+    for (let i = 0; i < this.redoArrStack.length; i++) {
+      this.redoStack += this.redoArrStack[i];
     }
-}
+  }
+};
+
+// console.log(output);
+// consolo.log(te.undoStack.length);
+// consolo.log(te.redoStack.length);
